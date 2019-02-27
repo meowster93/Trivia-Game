fdaba488562ee2c6a01a994b406f5cd216935668
meowster93 <cmendoza6277@yahoo.com>
2019-02-27 14:44:06 -0500

    progress but still so much to learning to do..

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,6 @@
+/*eslint-env browser*/
+/*global $*/
+
 $(document).ready(function () {
   //creating integer variables
 
@@ -5,33 +8,30 @@ $(document).ready(function () {
   var incorrectAnswer = 0;
   var unAnswered = 10;
 
+
+
   //created counter 
-  var count = 5;
-  var startCount;
+  var maxSecondsPerQuestion=15;
+  var count;
+  
 
   //created start button function so when button is clicked starts timer
   $("#start").on("click", function () {
-    clearInterval(startCount);
-    startCount = setInterval(decrement, 1000);
+    $("#start").hide();
+    nextPhase();
   });
   //creating functions needed
 
   //countdown initiates
   function decrement() {
+    
     count--;
     $("#timer").html(count);
-    $("#start").hide();
     if (count === 0) {
-      stop();
-      timesUp();
-      nextPhase();//create a function for next phase..
+      reactToTimeRunningOut();
+      nextPhase();
 
     }
-  }
-
-  //stoptimer
-  function stop() {
-    clearInterval(startCount);
   }
 
   //correct answer//
@@ -47,14 +47,133 @@ $(document).ready(function () {
     nextPhase()
   }
 
-  function timesUp() {
-    unAnswered--;
-    $("#unanswered-score").text("unanswered" + unAnswered);
-    nextPhase()
+  function reactToTimeRunningOut() {
+    if (count===0){
+      nextPhase();
+    }
   }
 
-  //questions and answers array
+  let activeQuestionIndex = -1;
+  var timerIntervalID;
 
+  function nextPhase() {
+    $("#activeQuestion").empty();
+    $("#activeAnswers").empty();
+    activeQuestionIndex++;
+    $('#activeQuestion').append(questions[activeQuestionIndex].prompt);
+    questions[activeQuestionIndex].availableAnswers.forEach(function (answer){
+      $('#activeAnswers').append($('<div>' + answer +'</div>'));  
+    })
+   count = maxSecondsPerQuestion;
+    if (timerIntervalID) clearInterval(timerIntervalID)
+    timerIntervalID = setInterval(decrement, 1000);
+  }
+
+
+  //questions and answers array
+  const questions = [
+    {
+      prompt: 'In Aladdin, what is the name of Jasmine’s pet tiger?',
+      availableAnswers: [
+        'Rajah', //correct
+        'Chetto',
+        'Bethaizor',
+        'Liseth'
+      ],
+      correctAnswerIndex: 0
+    },
+    {
+      prompt: 'In Peter Pan, Captain Hook had a hook on which one of his hands?',
+      availableAnswers: [
+        'His Right Foot',
+        'His Right',
+        'His Left',  //correct
+        'His Left Foot'
+      ],
+      correctAnswerIndex: 2
+    },
+    {
+      prompt: 'After being on earth, where did Hercules first meet his father Zeus?',
+      availableAnswers: [
+        'Mount Olympus',
+        'Prometheus Accadamy',
+        'In the Temple of Zeus',  //correct
+        'Phils Island'
+      ],
+      correctAnswerIndex: 2
+    },
+    {
+      prompt: 'In Toy Story, what game does the slinky play?',
+      availableAnswers: [
+        'Uno',
+        'Checkers',  //correct
+        'Zurg Game',
+        'Solitair'
+      ],
+      correctAnswerIndex: 1
+    },
+    {
+      prompt: 'In Hercules, Hades promised not to harm Megara provided that Hercules give up his strength. How long did he have to agree to give up his strength for?',
+      availableAnswers: [
+        '36 Hours',
+        '12 Hours',
+        '48 Hours',
+        '24 Hours'  //correct
+      ],
+      correctAnswerIndex: 3
+    },
+    {
+      prompt: 'What was the name of the dragon (god wanna be) in Mulan and who provided his voice?',
+      availableAnswers: [
+        'Mushu by Eddie Murphy',  //correct
+        'Mitsuki by Patrick Warburton',
+        'Nessie by Phil Harris',
+        'Pete by James Woods'
+      ],
+      correctAnswerIndex: 0
+    },
+    {
+      prompt: 'In Lady & the Tramp, by what name did Tony call Tramp? ',
+      availableAnswers: [
+        'Scooter',
+        'Butch',  //correct
+        'Copper',
+        'Lucky'
+      ],
+      correctAnswerIndex: 1
+    },
+    {
+      prompt: 'What was the name of the whale in Pinocchio?',
+      availableAnswers: [
+        'Laboon',
+        'Old Tom',
+        'Migaloo',
+        'Monstro'  //correct
+      ],
+      correctAnswerIndex: 3
+    },
+    {
+      prompt: 'In the Little Mermaid when Ariel sees Prince Eric, what type of celebration was happening on the ship??',
+      availableAnswers: [
+        'A Anniversary',
+        'A Birthday Party',  //correct
+        'Engagement Party',
+        'Graduation Party'
+      ],
+      correctAnswerIndex: 1
+    },
+    {
+      prompt: 'Which Disney movie was the first to be nominated for an Oscar?',
+      availableAnswers: [
+        'Beauty and the Beast',  //correct
+        'Hurcules',
+        'Dumbo',
+        'Toy Story'
+      ],
+      correctAnswerIndex: 0
+    }
+  ]
+  console.log(questions);
 
 
 
